20fef06945b7b3d27323285172da93e0ca212d82
Ross Middleton <5366498+R-Middleton@users.noreply.github.com>
2022-08-27 16:48:53 +0100

    header totals started

--- src/components/budget/header/budgetHeader.tsx
+++ src/components/budget/header/budgetHeader.tsx
@@ -1,10 +1,11 @@
 import HeaderCalendar from './headerCalendar';
+import HeaderTotals from './headerTotals';
 
 const BudgetHeader = () => {
   return (
     <div className='bg-slate-200 grid-in-header flex text-black gap-6'>
       <HeaderCalendar />
-      <div>Header Total Component</div>
+      <HeaderTotals />
     </div>
   );
 };
---
+++ src/components/budget/header/headerTotals.tsx
@@ -0,0 +1,9 @@
+const HeaderTotals = () => {
+  return (
+    <div className=' bg-slate-300 rounded-md m-2 grid items-center '>
+      <div>£0.00</div>
+    </div>
+  );
+};
+
+export default HeaderTotals;
